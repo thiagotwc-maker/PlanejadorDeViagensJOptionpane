265c7aa0c1e751f2227e29c9fd1b1a703cd083a6
Thiago Wesley Cunha <thiagotwc@gmail.com>
2026-04-24 21:50:34 -0300

    Correção de bugs não corrigida

--- src/planejadordeviagem/PlanejadorDeViagem.java
+++ src/planejadordeviagem/PlanejadorDeViagem.java
@@ -15,6 +15,7 @@ public class PlanejadorDeViagem {
         opcoes();
     }
 
+    // Aqui e uma função com o menu inicial.
     public static void menu() {
         String[] opcoes = {"Realizar Viagem", "Sair"};
         int selecao = JOptionPane.showOptionDialog(null, "Escolha Uma Opção", "CVC Turismo",
@@ -24,34 +25,42 @@ public class PlanejadorDeViagem {
         }
     }
 
+    // Metodo com todas as verificações e condições
     public static void opcoes() {
 
-        String nome = JOptionPane.showInputDialog("Nome e Sobrenome");
+        long dias = 0;
+        String data = null;
+        String nome = null;
+        int numero = 0;
+        double rico = 0;
+
+        // aqui recebe o nome do usuario.
+        nome = JOptionPane.showInputDialog("Nome e Sobrenome");
         if (nome.isBlank()) {
             JOptionPane.showMessageDialog(null, "Não Pode Estar Vazio.");
         }
-        
-        
-        
-        String data = JOptionPane.showInputDialog("Data Da Viagem");
 
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate converter = LocalDate.parse(data, formato);
-        LocalDate dataFutura = LocalDate.parse(data, formato);
-        
-        LocalDate hoje = LocalDate.now();
-        long dias = ChronoUnit.DAYS.between(hoje, dataFutura);
+        // Aqui pega a data da viagem do usuario desde que seja válida.
+        data = JOptionPane.showInputDialog("Data Da Viagem");
+        try {
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate converter = LocalDate.parse(data, formato);
+            LocalDate dataFutura = LocalDate.parse(data, formato);
+            LocalDate hoje = LocalDate.now();
 
-        JOptionPane.showMessageDialog(null, "Data Inválida: " + data);
+            dias = ChronoUnit.DAYS.between(hoje, dataFutura);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Data Inválida: ");
+        }
 
         if (data.isBlank()) {
             JOptionPane.showMessageDialog(null, "Não Pode Estar Vazio.");
         }
-        
-        
 
+        // Aqui pega a quantidade de dias que o usuario vai digitar.
         String dia = JOptionPane.showInputDialog("Quantidade De Dias.");
-        int numero = Integer.parseInt(dia);
+        numero = Integer.parseInt(dia);
 
         if (dia.isBlank()) {
             JOptionPane.showMessageDialog(null, "Não Pode Estar Vazio.");
@@ -59,10 +68,10 @@ public class PlanejadorDeViagem {
         if (numero < 0) {
             JOptionPane.showMessageDialog(null, "Numero inválido( numero negativo).");
         }
-        
 
+        // Aqui vai receber o valor que o usuario pretende gastar na viagem.
         String valor = JOptionPane.showInputDialog("Valor Gasto Por Dia.");
-        double rico = Double.parseDouble(valor);
+        rico = Double.parseDouble(valor);
 
         if (valor.isBlank()) {
             JOptionPane.showMessageDialog(null, "Não Pode Estar Vazio.");
@@ -70,11 +79,11 @@ public class PlanejadorDeViagem {
         if (rico < 0) {
             JOptionPane.showMessageDialog(null, "Numero inválido( numero negativo).");
         }
-        
-        
+
+        //Aqui exibe todos os dados coletados anteriormente.
         JOptionPane.showMessageDialog(null, "Nome: " + nome + "\nData: " + data + "\nDias: " + numero + "\nValo Por Dia: " + rico);
 
         double resultado = numero * rico;
-        JOptionPane.showMessageDialog(null, nome + "Sua Viagem Será: " + data + "\nFaltam: " + dias + "\nO Custo Total É: " + resultado);
+        JOptionPane.showMessageDialog(null, nome + "  Sua Viagem Será: " + data + "\nFaltam: " + dias + "\nO Custo Total É: " + resultado);
     }
 }
